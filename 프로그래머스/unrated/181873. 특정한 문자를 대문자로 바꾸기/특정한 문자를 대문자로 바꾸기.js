4ea65add841b6baa9ddfe71bd21427fae262b652
yeonhub <128147281+yeonhub@users.noreply.github.com>
2023-10-12 00:01:40 +0900

    [level unrated] Title: 특정한 문자를 대문자로 바꾸기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.js
+++ 프로그래머스/unrated/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.js
@@ -1,6 +1,3 @@
 function solution(my_string, alp) {
-    var answer = new RegExp(alp, 'g');
-    answer = my_string.replaceAll(answer, alp.toUpperCase())
-
-    return answer
+    return my_string.replaceAll(alp, alp.toUpperCase())
 }
