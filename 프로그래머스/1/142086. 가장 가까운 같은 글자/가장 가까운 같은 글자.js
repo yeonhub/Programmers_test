bda217162d0db79fb5bc6b21b83c71a35ebf3f12
yeonhub <128147281+yeonhub@users.noreply.github.com>
2023-11-06 23:03:02 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 4.55 ms, Memory: 37.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -0,0 +1,17 @@
+function solution(s) {
+    var answer = [];
+    for (let i = 0; i < s.length; i++) {
+        let found = false;
+        for (let j = i - 1; j >= 0; j--) {
+            if (s[i] === s[j]) {
+                answer.push(i - j);
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            answer.push(-1);
+        }
+    }
+    return answer;
+}
