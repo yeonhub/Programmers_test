672b1ec5435886ac0e397920fc44e1ff43332871
yeonhub <lsy3237@gmail.com>
2024-03-03 00:46:31 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.37 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -0,0 +1,13 @@
+function solution(numbers) {
+    const answer = [];
+    for (let i = 0; i < numbers.length - 1; i++) {
+        for (let j = i + 1; j < numbers.length; j++) {
+            const sum = numbers[i] + numbers[j];
+
+            if (!answer.includes(sum)) {
+                answer.push(sum);
+            }
+        }
+    }
+    return answer.sort((a, b) => a - b);
+}
